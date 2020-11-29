8401a8552be3e334bec5d72747d18661ae493f6b
Markos Rafael <escalante.markos18@gmail.com>
2020-11-28 23:19:40 -0800

    create ajax method to retrieve open weather map API

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 const searchCity = document.getElementById("search-city");
 const submitBtn = document.getElementById("submit-button");
 const clearHstry = document.getElementById("clear-history");
-const 
+const currentCty = document.getElementById("city-name");
+const currentDte = document.getElementById("current-date");
+const temp = document.getElementById("temperature");
+const humidity = document.getElementById("humidity");
+const windSpd = document.getElementById("wind-speed");
+const uvIndx = document.getElementById("uv-index");
+const foreCast = document.getElementById("forecast");
+
+
+function cityWeather(cityName) {
+    const APIKEY = "3024ac46a81fa285ebdedcec74633cb2"
+
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    $.ajax({
+    url: queryURL,
+    method: 'GET'
+    }).then(function (response) {
+    $("#weatherIcon").empty();
+}
